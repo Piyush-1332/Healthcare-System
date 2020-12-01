6c482f709be5c1cdeb78a4d6878dfd967e30e51e
Piyush-1332 <43117334+Piyush-1332@users.noreply.github.com>
2020-12-01 14:17:54 +0530

    Home Page Added

--- FrontEnd/hms/src/app/app-routing.module.ts
+++ FrontEnd/hms/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppHomeComponent } from './components/shared/home/app-home/app-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AppHomeComponent}
+  // {path:'bookAppointment',component:},
+  // {path:'bookAppointment',component:},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
