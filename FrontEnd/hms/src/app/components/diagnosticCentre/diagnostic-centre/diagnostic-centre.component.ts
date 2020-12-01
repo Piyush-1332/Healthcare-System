db90e4584619c46c812fdb0e832a970d18aeebaf
Piyush-1332 <43117334+Piyush-1332@users.noreply.github.com>
2020-12-01 11:32:13 +0530

    Frontend Structure Added

---
+++ FrontEnd/hms/src/app/components/diagnosticCentre/diagnostic-centre/diagnostic-centre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnostic-centre',
+  templateUrl: './diagnostic-centre.component.html',
+  styleUrls: ['./diagnostic-centre.component.css']
+})
+export class DiagnosticCentreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
